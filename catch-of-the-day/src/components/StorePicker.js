e50549f8c2db0121646e0e1fd1ea119976b6606f
Jake Rayson <web@growdigital.org>
2017-10-05 12:16:04 +0100

    using eslint. moving along nicely.

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { getFunName } from '../helpers';
+
+class StorePicker extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
+  goToStore(event) {
+    event.preventDefault();
+    console.log('You Changed the URL');
+    // first grab the text form the box
+    const storeId = this.storeInput.value;
+    console.log('Going to URL');
+    // second transtion from / to /store/:storeId
+    this.context.router.transitionTo(`/store/${storeId}`);
+  }
+
+  render() {
+    // Any where else
+    return (
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
+        <button type="submit">Visit Store →</button>
+      </form>
+    )
+  }
+}
+
+export default StorePicker;
